cbaacad2c27b27ab6eaf7a90061688b3ec1bb46c
Or Perry <orperry@gmail.com>
2025-10-01 01:22:41 +0300

    styling

--- src/services/five-in-square.service.ts
+++ src/services/five-in-square.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FiveInSquareService {
   public initArrays(): number[][] {
-    // Create a 5x5 array filled with zeros
-    return Array.from({ length: 5 }, () => Array(5).fill(0));
+    // Create a 5x5 array filled with random numbers between 1 and 9
+    return Array.from({ length: 5 }, () =>
+      Array.from({ length: 5 }, () => Math.floor(Math.random() * 9) + 1)
+    );
   }
 }
